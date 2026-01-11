a788a76282216a2a6f3a284e84674e7b14365a93
Vu Dai Duong <you@example.com>
2026-01-11 23:01:11 +0700

    Initial commit

---
+++ src/main/java/org/example/hung_hypebeast_backend/HungHypebeastBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.hung_hypebeast_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HungHypebeastBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HungHypebeastBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/hung_hypebeast_backend/HungHypebeastBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hung_hypebeast_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungHypebeastBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
